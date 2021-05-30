ae2ef112e3a7f6b8e8dcd1720034dff2787c0ca4
DaRiv94 <40576435+DaRiv94@users.noreply.github.com>
2021-05-30 01:31:11 +0000

    Deploying to gh-pages from @ DaRiv94/fgr-secure-api@49644e8f72b45abe30501117bf4b58585b1685ae 🚀

--- src/components/WelcomUser.js
+++
@@ -1,8 +0,0 @@
-import { useMsal} from "@azure/msal-react";
-
-export default function WelcomeUser() {
-    const { accounts } = useMsal();
-    const username = accounts[0].username;
-  
-    return <p>Welcome, {username}</p>
-  }
